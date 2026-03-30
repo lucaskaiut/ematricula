1ea54c77a73b5faa8af19398b4d8b1abff73f61c
Lucas Kaiut <lucas.kaiut@gmail.com>
2026-03-30 17:08:16 -0300

    force json response

---
+++ api/app/Http/Middleware/ForceJsonResponse.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ForceJsonResponse
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $request->headers->set('Accept', 'application/json');
+        $request->setRequestFormat('json');
+
+        return $next($request);
+    }
+}
+
--- api/bootstrap/app.php
+++ api/bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->appendToGroup('api', \App\Http\Middleware\ForceJsonResponse::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
